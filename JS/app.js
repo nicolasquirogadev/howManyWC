9cc81ed0d44dcb4985905b949348cbf5881cc79e
zicolino <167160363+zicolino@users.noreply.github.com>
2024-04-24 20:46:32 -0300

    Algoritmo terminado

---
+++ JS/app.js
@@ -0,0 +1,97 @@
+let edad = parseFloat(prompt("Cuantos años tenés?"))
+
+let nacimiento = (2024 - edad)
+{
+    console.log(nacimiento)
+}
+
+if (nacimiento<=1930) {
+    alert("Impresionante! Usted ha las visto 22 Copas del Mundo.")
+}
+else if ((nacimiento => 1931)&&(nacimiento <= 1934)) {
+    alert("Impresionante! Usted ha visto 21 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1935)&&(nacimiento <= 1938)) {
+    alert("Impresionante! Usted ha visto 20 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1939)&&(nacimiento <= 1949)) {
+    alert("Impresionante! Usted ha visto 19 Copas del Mundo.")
+}
+
+else if ((nacimiento=>1950)&&(nacimiento <= 1953)) {
+    alert("Impresionante! Usted ha visto 19 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1954)&&(nacimiento <= 1957)) {
+    alert("Impresionante! Usted ha visto 18 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1958)&&(nacimiento <= 1961)) {
+    alert("Impresionante! Usted ha visto 17 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1962)&&(nacimiento <= 1965)) {
+    alert("Impresionante! Usted ha visto 16 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1966)&&(nacimiento <= 1969)) {
+    alert("Impresionante! Usted ha visto 15 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1970)&&(nacimiento <= 1973)) {
+    alert("Impresionante! Usted ha visto 14 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1974)&&(nacimiento <= 1977)) {
+    alert("Impresionante! Usted ha visto 13 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1978)&&(nacimiento <= 1981)) {
+    alert("Impresionante! Usted ha visto 12 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1982)&&(nacimiento <= 1985)) {
+    alert("Impresionante! Usted ha visto 11 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1986)&&(nacimiento <= 1989)) {
+    alert("Impresionante! Usted ha visto 10 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1990)&&(nacimiento <= 1993)) {
+    alert("Increible! Usted ha visto 9 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1994)&&(nacimiento <= 1997)) {
+    alert("Increible! Usted ha visto 8 Copas del Mundo.")
+}
+
+else if ((nacimiento => 1998)&&(nacimiento <= 2001)) {
+    alert("Bien! Usted ha visto 7 Copas del Mundo.")
+}
+
+else if ((nacimiento => 2002)&&(nacimiento <= 2005)) {
+    alert("Bien! Usted ha visto 6 Copas del Mundo.")
+}
+
+else if ((nacimiento => 2006)&&(nacimiento <= 2009)) {
+    alert("Bueno, al menos usted ha visto 5 Copas del Mundo.")
+}
+
+else if ((nacimiento => 2010)&&(nacimiento <= 2013)) {
+    alert("Bueno, al menos usted ha visto 4 Copas del Mundo.")
+}
+
+else if ((nacimiento => 2014)&&(nacimiento <= 2017)) {
+    alert("Bueno, al menos usted ha visto 3 Copas del Mundo.")
+}
+
+else if ((nacimiento => 2018)&&(nacimiento <= 2021)) {
+    alert("Bueno, al menos usted ha visto 2 Copas del Mundo.")
+}
+
+else if (nacimiento => 2022) {
+    alert("Ja! Usted ha visto solo 1 Copa del Mundo.")
+}
